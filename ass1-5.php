be11e44840dc88eb244091733d2732f9146be55c
Rasha-Kamel <rashakamel42020@gmail.com>
2024-02-18 20:16:42 +0200

    assignment from lesson1 to 5

---
+++ ass1-5.php
@@ -0,0 +1,43 @@
+<?php
+//Task(1)
+/*
+comment for 
+muliply lines
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en" <?php charset="UTF-8" ?>>
+  <head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo " My First PHP Page"; ?></title>
+  </head>
+  <body>
+    <div><?php print "we Love"; ?></div>
+    <div><?php print "Elzero Channel"; ?></div>
+  </body>
+</html>
+
+
+<?php
+//Task(2) methods of vomment code
+
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';  
+/* echo 'Prevent Me From Running Please';*/
+
+//Task(3) 
+/* 
+My Application
+Version 1.0
+Created By Elzero
+*/
+
+//Task(4) 
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+?>
